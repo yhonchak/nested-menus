55fa47027f84f7d00eae6696117da9a9ced09685
Yaroslav Honchak <yaroslav.honchak@gmail.com>
2024-02-15 13:31:56 +0200

    feat(app): add the bootstrap's navbar tag

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,5 +1,5 @@
-import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -12,5 +12,14 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
+  });
+
+  it('should contain the bootstrap\'s navbar', () => {
+    const fixture: ComponentFixture<AppComponent> = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+
+    const compiled: HTMLElement = fixture.nativeElement as HTMLElement;
+
+    expect(compiled.querySelector('nav.navbar')).toBeTruthy()
   });
 });
