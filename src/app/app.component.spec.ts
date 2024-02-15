7fe8ccda2557ef642a248071f2fc9c23bb79afc5
Yaroslav Honchak <yaroslav.honchak@gmail.com>
2024-02-15 12:51:54 +0200

    chore(app): remove redundant property `title`
    this property was generated by `angular-cli` by default

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -13,10 +13,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have the 'nested-menus' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('nested-menus');
-  });
 });
