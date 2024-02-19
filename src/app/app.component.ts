f6941fbb25419efb3cec6be852c41054b953d7bd
Yaroslav Honchak <yaroslav.honchak@gmail.com>
2024-02-19 18:21:43 +0200

    feat(app): add the menu items data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,47 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MenuComponent } from './layout/menu/menu.component';
+import { MenuItem } from './layout/menu/menu-item/menu-item.component';
+
+const menu: MenuItem[] = [
+  {
+    title: 'Home',
+    icon: 'bi-house',
+    route: '/home',
+    children: []
+  },
+  {
+    title: 'Notifications',
+    icon: 'bi-bell',
+    route: '/notifications',
+    children: []
+  },
+  {
+    title: 'Group',
+    icon: 'bi-collection',
+    route: '',
+    children: [
+      {
+        title: 'First Group',
+        icon: 'bi-collection-fill',
+        route: '/first-group',
+        children: []
+      },
+      {
+        title: 'Second Group',
+        icon: 'bi-collection-play',
+        route: '/second-group',
+        children: []
+      },
+    ]
+  },
+  {
+    title: 'Link',
+    icon: 'bi-link',
+    route: '/link',
+    children: []
+  },
+];
 
 @Component({
   selector: 'app-root',
