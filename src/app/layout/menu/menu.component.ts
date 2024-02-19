8d4066a1e82d632b3e7b25392d7df08bf696f7b7
Yaroslav Honchak <yaroslav.honchak@gmail.com>
2024-02-19 14:35:00 +0200

    feat(menu): add `menu-item` to the component template

--- src/app/layout/menu/menu.component.ts
+++ src/app/layout/menu/menu.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MenuItemComponent } from './menu-item/menu-item.component';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [],
+  imports: [MenuItemComponent],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.scss'
 })
