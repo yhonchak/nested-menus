037717e97dfd13621db4a8ceb16bda05d17ec2e7
Yaroslav Honchak <yaroslav.honchak@gmail.com>
2024-02-21 16:26:38 +0200

    feat(menu): add more child items to menu array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,16 +22,42 @@ const MENU: MenuItem[] = [
     route: '',
     children: [
       {
-        title: 'First Group',
+        title: '1st Screen',
         icon: 'bi-collection-fill',
         route: '/first-group',
         children: []
       },
       {
-        title: 'Second Group',
+        title: '2nd Screen',
         icon: 'bi-collection-play',
         route: '/second-group',
         children: []
+      },
+      {
+        title: '3rd Level Group',
+        icon: 'bi-list-nested',
+        route: '',
+        children: [
+          {
+            title: '3rd Level Screen',
+            icon: 'bi-3-circle',
+            route: '/link',
+            children: []
+          },
+          {
+            title: '4th Level Group',
+            icon: 'bi-4-circle',
+            route: '',
+            children: [
+              {
+                title: '4th Level Screen',
+                icon: 'bi-4-square',
+                route: '/notifications',
+                children: []
+              }
+            ]
+          },
+        ]
       },
     ]
   },
